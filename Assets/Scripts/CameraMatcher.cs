5fc89fcc0853bf1d8479cd3c5b76355ddb376ead
Victor Junaidy <victor.junaidy@gmail.com>
2021-05-01 00:05:29 +0700

    Minor

--- Assets/Scripts/CameraMatcher.cs
+++ Assets/Scripts/CameraMatcher.cs
@@ -1,11 +1,23 @@
+using TMPro;
 using UnityEngine;
 
 public class CameraMatcher : MonoBehaviour
 {
-    public Camera camera;
+    public GameObject m_gpsUIText;
+
+    private Transform m_mainCamera;
+
+    void Awake()
+    {
+        m_mainCamera = GameObject.FindGameObjectsWithTag("MainCamera")[0].transform;
+    }
  
     void Update()
     {
-        transform.rotation = camera.transform.rotation;
+        transform.rotation = Quaternion.Euler(0, m_mainCamera.transform.localEulerAngles.y, 0);
+        // transform.rotation = GetComponent<Camera>().transform.rotation;
+
+        m_gpsUIText.GetComponent<TMP_Text>().text =
+                "camera localEulerAngles.y: " + m_mainCamera.transform.localEulerAngles.y;
     }
 }
