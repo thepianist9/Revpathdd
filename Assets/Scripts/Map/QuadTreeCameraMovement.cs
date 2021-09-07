3665fbb40c95b8af61731cc40e2c7ad312fb0d27
Victor Junaidy <victor.junaidy@gmail.com>
2021-09-07 21:07:29 +0200

    Cleanup QuadTreeCameraMovement + handle drag on UI

--- Assets/Scripts/Map/QuadTreeCameraMovement.cs
+++ Assets/Scripts/Map/QuadTreeCameraMovement.cs
@@ -2,9 +2,9 @@
 using Mapbox.Unity.Map;
 using Mapbox.Unity.Utilities;
 using Mapbox.Utils;
+using System;
 using UnityEngine;
 using UnityEngine.EventSystems;
-using System;
 
 namespace HistocachingII
 {
@@ -63,7 +63,7 @@ namespace HistocachingII
 
 		public void Update()
 		{
-			if (Input.GetMouseButtonDown(0) && EventSystem.current.IsPointerOverGameObject())
+			if (Input.GetMouseButtonDown(0) && EventSystem.current.IsPointerOverGameObject(Input.GetTouch(0).fingerId))
 			{
 				_dragStartedOnUI = true;
 			}
@@ -101,32 +101,10 @@ namespace HistocachingII
 			{
 				if (!_dragStartedOnUI)
 					if (Input.touchSupported && Input.touchCount > 0)
-					{
 						HandleTouch();
-					}
 					else
-					{
 						m_IsTouchReset = true;
-						HandleMouseAndKeyBoard();
-					}
 			}
-		}
-
-		void HandleMouseAndKeyBoard()
-		{
-			// zoom
-			float scrollDelta = 0.0f;
-			scrollDelta = Input.GetAxis("Mouse ScrollWheel");
-			ZoomMapUsingTouchOrMouse(scrollDelta);
-
-			//pan keyboard
-			float xMove = Input.GetAxis("Horizontal");
-			float zMove = Input.GetAxis("Vertical");
-
-			PanMapUsingKeyBoard(xMove, zMove);
-
-			//pan mouse
-			PanMapUsingTouchOrMouse();
 		}
 
 		void HandleTouch()
@@ -137,7 +115,7 @@ namespace HistocachingII
 			{
 				case 1:
 					if (m_IsTouchReset)
-						PanMapUsingTouchOrMouse();
+						PanMapUsingTouch();
 					break;
 				case 2:
 					{
@@ -158,14 +136,14 @@ namespace HistocachingII
 						// Find the difference in the distances between each frame.
 						zoomFactor = 0.01f * (touchDeltaMag - prevTouchDeltaMag);
 					}
-					ZoomMapUsingTouchOrMouse(zoomFactor);
+					ZoomMapUsingTouch(zoomFactor);
 					break;
 				default:
 					break;
 			}
 		}
 
-		void ZoomMapUsingTouchOrMouse(float zoomFactor)
+		void ZoomMapUsingTouch(float zoomFactor)
 		{
 			var zoom = Mathf.Max(0.0f, Mathf.Min(_mapManager.Zoom + zoomFactor * _zoomSpeed, 21.0f));
 			if (Math.Abs(zoom - _mapManager.Zoom) > 0.0f)
@@ -174,35 +152,7 @@ namespace HistocachingII
 			}
 		}
 
-		void PanMapUsingKeyBoard(float xMove, float zMove)
-		{
-			if (Math.Abs(xMove) > 0.0f || Math.Abs(zMove) > 0.0f)
-			{
-				// Get the number of degrees in a tile at the current zoom level.
-				// Divide it by the tile width in pixels ( 256 in our case)
-				// to get degrees represented by each pixel.
-				// Keyboard offset is in pixels, therefore multiply the factor with the offset to move the center.
-				float factor = _panSpeed * (Conversions.GetTileScaleInDegrees((float)_mapManager.CenterLatitudeLongitude.x, _mapManager.AbsoluteZoom));
-
-				var latitudeLongitude = new Vector2d(_mapManager.CenterLatitudeLongitude.x + zMove * factor * 2.0f, _mapManager.CenterLatitudeLongitude.y + xMove * factor * 4.0f);
-
-				_mapManager.UpdateMap(latitudeLongitude, _mapManager.Zoom);
-			}
-		}
-
-		void PanMapUsingTouchOrMouse()
-		{
-			if (_useDegreeMethod)
-			{
-				UseDegreeConversion();
-			}
-			else
-			{
-				UseMeterConversion();
-			}
-		}
-
-		void UseMeterConversion()
+		void PanMapUsingTouch()
 		{
 			if (Input.GetMouseButtonUp(1))
 			{
@@ -268,72 +218,6 @@ namespace HistocachingII
 					_origin = _mousePosition;
 				}
 			}
-		}
-
-		void UseDegreeConversion()
-		{
-			if (Input.GetMouseButton(0) && !EventSystem.current.IsPointerOverGameObject())
-			{
-				var mousePosScreen = Input.mousePosition;
-				//assign distance of camera to ground plane to z, otherwise ScreenToWorldPoint() will always return the position of the camera
-				//http://answers.unity3d.com/answers/599100/view.html
-				mousePosScreen.z = _referenceCamera.transform.localPosition.y;
-				_mousePosition = _referenceCamera.ScreenToWorldPoint(mousePosScreen);
-
-				if (_shouldDrag == false)
-				{
-					_shouldDrag = true;
-					_origin = _referenceCamera.ScreenToWorldPoint(mousePosScreen);
-				}
-			}
-			else
-			{
-				_shouldDrag = false;
-			}
-
-			if (_shouldDrag == true)
-			{
-				m_ScreenManager.SetStickyMyLocation(false);
-
-				var changeFromPreviousPosition = _mousePositionPrevious - _mousePosition;
-				if (Mathf.Abs(changeFromPreviousPosition.x) > 0.0f || Mathf.Abs(changeFromPreviousPosition.y) > 0.0f)
-				{
-					_mousePositionPrevious = _mousePosition;
-					var offset = _origin - _mousePosition;
-
-					if (Mathf.Abs(offset.x) > 0.0f || Mathf.Abs(offset.z) > 0.0f)
-					{
-						if (null != _mapManager)
-						{
-							// Get the number of degrees in a tile at the current zoom level.
-							// Divide it by the tile width in pixels ( 256 in our case)
-							// to get degrees represented by each pixel.
-							// Mouse offset is in pixels, therefore multiply the factor with the offset to move the center.
-							float factor = _panSpeed * Conversions.GetTileScaleInDegrees((float)_mapManager.CenterLatitudeLongitude.x, _mapManager.AbsoluteZoom) / _mapManager.UnityTileSize;
-
-							var latitudeLongitude = new Vector2d(_mapManager.CenterLatitudeLongitude.x + offset.z * factor, _mapManager.CenterLatitudeLongitude.y + offset.x * factor);
-							_mapManager.UpdateMap(latitudeLongitude, _mapManager.Zoom);
-						}
-					}
-					_origin = _mousePosition;
-				}
-				else
-				{
-					if (EventSystem.current.IsPointerOverGameObject())
-					{
-						return;
-					}
-					_mousePositionPrevious = _mousePosition;
-					_origin = _mousePosition;
-				}
-			}
-		}
-
-		private Vector3 getGroundPlaneHitPoint(Ray ray)
-		{
-			float distance;
-			if (!_groundPlane.Raycast(ray, out distance)) { return Vector3.zero; }
-			return ray.GetPoint(distance);
 		}
 	}
 }
